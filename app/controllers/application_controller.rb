feb2926cb7d150a2b5f2d7cfc38e802a30192ec6
Oswaldo Zavala <oszavala@gmail.com>
2019-08-09 14:58:26 -0500

    Integrating the cart into the application

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::Base
+
+  def initialize_cart
+    @cart = Cart.build_from_hash session
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   root 'products#index'
   resources :products
+
+  resource :cart, only: [ :show ] do
+    post :add, path: "add/:id"
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
